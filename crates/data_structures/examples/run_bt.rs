2f3e1aae416befc1119bef6062d5b967b6d90c64
Marek Kancir <kancir.marek@gmail.com>
2025-09-23 17:08:59 +0200

    wip: simple BT

--- crates/data_structures/examples/run_bt.rs
+++ crates/data_structures/examples/run_bt.rs
@@ -1,5 +1,19 @@
 use data_structures::binary_tree::bt;
 
 fn main() {
-    bt::bt();
+    let mut root = bt::Node::new(1);
+
+    root.left = Some(Box::new(bt::Node::new(2)));
+
+    println!("{}", &root.value);
+    println!("{}", &root.left.as_ref().unwrap().value);
+    
+    match &root.right {
+        Some(node) => println!("Right node value: {}", node.value),
+        _ => println!("Right node is empty")
+    }
+
+    println!("height of root: {}", root.height());
+    println!("depth of root.left: {}", root.depth(&root.left).unwrap_or(-9));
+    println!("depth of root.right: {}", root.depth(&root.right).unwrap_or(-999));
 }
--- crates/data_structures/src/binary_tree/bt.rs
+++ crates/data_structures/src/binary_tree/bt.rs
@@ -1,3 +1,31 @@
-pub fn bt() {
-    println!("Binary Tree")
+type Link<T> = Option<Box<Node<T>>>;
+
+pub struct Node<T> {
+    pub value: T,
+    pub left: Link<T>,
+    pub right: Link<T>
+}
+
+impl<T> Node<T> {
+    pub fn new(value: T) -> Node<T> {
+        Node{value:value, left: None, right: None}
+    }
+
+    pub fn height(&self) -> i32 {
+        let left_height = self.left.as_ref().map_or(0, |node| node.height() + 1);
+        let right_height = self.right.as_ref().map_or(0, |node| node.height() + 1);
+
+        left_height.max(right_height)
+    }
+    // TODO: Implement proper `depth` function
+    // - Depth = number of edges from ROOT -> specific NODE (not value).
+    // - Given the root (self), find this exact node in the tree.
+    // - Must search by node identity (pointer), not by value, since multiple nodes can hold the same value 
+    //   (e.g., two distinct nodes with value 4). Use `std::ptr::eq` to check.
+    pub fn depth(&self, node: &Link<T>) -> Option<i32> {
+        match node {
+            Some(_) => return Some(-99),
+            None => return None
+        };
+    }
 }
